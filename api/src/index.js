ef86d00b283efe6254bd5fe49e4499b9865f3072
LiamAttClarke <liam.att.clarke@gmail.com>
2019-06-12 01:46:01 -0400

    Updated db configuration

--- api/src/index.js
+++ api/src/index.js
@@ -1,21 +1,28 @@
 const express = require('express')
-const app = express()
+const { Client, Pool } = require('pg')
+
 const PORT = 80
-const DB_NAME = process.env.DB_NAME
 
-app.get('/', (request, response) => {
-  console.log(request)
-  response.send('Hello World!')
+const app = express()
+const pgPool = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
 })
 
-app.get('/test', (request, response) => {
-  console.log(request)
-  response.send('Have a cookie!')
+app.get('/', async (request, response) => {
+  response.send('Hello!')
 })
 
-app.get('/db_data', (request, response) => {
-  console.log(request)
-  response.send(`DB_NAME: ${DB_NAME}`)
+app.get('/articles', async (request, response) => {
+  const queryResponse = await pgPool.query('SELECT * FROM articles')
+    .catch(e => {
+      console.log(e)
+      response.sendStatus(500)
+    })
+  await pgPool.end()
+  response.send(queryResponse.rows)
 })
 
 app.listen(PORT, () => {
